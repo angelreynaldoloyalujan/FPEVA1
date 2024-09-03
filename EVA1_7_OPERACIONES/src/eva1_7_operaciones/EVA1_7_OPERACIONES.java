17281147298ad3a99f85dbd99b13ccf4f449df73
angelreynaldoloyalujan <L24550331@chihuahua2.tecnm.mx>
2024-09-03 16:48:41 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //OPERACIONES MATEMATICAS
+        //SUMA Y RESTA
+        int x = 10;// deaclaracion y asignacion
+        int y = 5;
+        int suma , resta , divi , multi , pot , raiz ;
+        suma = x + y;
+        System.out.println("suma de x+y");
+        System.out.println(suma);
+        
+        resta = x - y ;
+        System.out.println("resta de x-y");
+        System.out.println(resta);
+        
+        multi = x * y;
+        System.out.println("multi de x*y");
+        System.out.println(multi);
+        
+        divi = x / y;
+        System.out.println("divi de x/y");
+        System.out.println(divi);
+        
+        x=5;
+        y=2;
+        System.out.println("division x(5) / y(2)");
+        System.out.println(divi);
+        
+        double val1=11;
+        double val2=3;
+        double diviD;
+        diviD = val1/val2;
+        System.out.println("division entre #decimales");
+    }
+}
+    
+    
+
